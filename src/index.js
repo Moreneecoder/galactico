954c739eb8291a9d79aaba535081c15a87e64d3b
Morenikeji Bello <caeserselassie@gmail.com>
2021-08-27 01:16:54 +0100

    Setup webpack-dev-server

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const messageEl = document.createElement('div');
+messageEl.classList.add('text-success', 'fw-bolder')
 messageEl.textContent = 'I was put here by JavaScript!';
 document.body.appendChild(messageEl);
