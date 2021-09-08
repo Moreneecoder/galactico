ffadc8ac0490493d80ec28499faeb43233d9902b
Morenikeji Bello <caeserselassie@gmail.com>
2021-09-08 14:07:22 +0100

    Create player sprite

--- src/Config/config.js
+++ src/Config/config.js
@@ -5,4 +5,14 @@ export default {
   parent: 'phaser-example',
   width: 800,
   height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+        // gravity: { y: 300 },
+        gravity: { x: 0, y: 0 },
+        debug: false
+    }
+  },
+  pixelArt: true,
+  roundPixels: true
 };
--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -1,5 +1,7 @@
 import 'phaser';
-import Logo from '../assets/logo.png';
+// import Logo from '../assets/logo.png';
+import PlayerImg from '../assets/hero.png'
+import { Player } from '../Objects/Entities';
 
 class GameScene extends Phaser.Scene {
   constructor() {
@@ -7,13 +9,31 @@ class GameScene extends Phaser.Scene {
   }
 
   preload() {
-    // load images
-    this.load.image('logo', Logo);
+    this.load.spritesheet("player", PlayerImg, {
+      frameWidth: 300,
+      frameHeight: 200
+    });
+
   }
 
   create() {
-    this.add.image(400, 300, 'logo');
+
+    this.anims.create({
+      key: "player",
+      frames: this.anims.generateFrameNumbers("player"),
+      frameRate: 20,
+      repeat: -1
+    });
+
+    this.player = new Player(
+      this,
+      this.game.config.width * 0.5,
+      this.game.config.height * 0.7,
+      "player"
+    );
+
   }
+
 }
 
 export default GameScene;
