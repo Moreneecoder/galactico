598158a237ecfebb25801b0c6a6bf4d49949d209
Morenikeji Bello <caeserselassie@gmail.com>
2021-09-12 03:18:42 +0100

    Fix Phaser not defined error in Player and Enemy classes

--- src/Objects/PlayerLaser.js
+++ src/Objects/PlayerLaser.js
@@ -1,10 +1,10 @@
-import Entity from "./Entities";
+import Entity from './Entities';
 
 class PlayerLaser extends Entity {
-    constructor(scene, x, y) {
-      super(scene, x, y, 'playerLaser');
-      this.body.velocity.y = -200;
-    }
+  constructor(scene, x, y) {
+    super(scene, x, y, 'playerLaser');
+    this.body.velocity.y = -200;
+  }
 }
 
-export default PlayerLaser
+export default PlayerLaser;
