5ec0fe3f703111ad5eb514142067f46dc20c28dd
Morenikeji Bello <caeserselassie@gmail.com>
2021-09-15 23:12:31 +0100

    Write unit test for Leaderboard store function

--- tests/ldboard.test.js
+++ tests/ldboard.test.js
@@ -1,6 +1,24 @@
-// import { expect, it } from '@jest/globals'
 import * as ldAPI from '../src/API/ldboard'
+import 'regenerator-runtime'
 
-it('checks', () => {
-    expect(typeof ldAPI.url).toBe('string')
+jest.mock('../src/API/ldboard')
+
+describe('Store new score', () => {
+    ldAPI.store = jest.fn().mockImplementation((user, score) => {        
+        return {
+            user, score
+        }
+    })
+
+    it('expects first argument to be a string', async () => {
+      const response = await ldAPI.store('A name', 20);
+      expect(typeof response.user).toBe('string')
+    })
+
+    it('expects second argument to be a number', async () => {
+        const response = await ldAPI.store('A name', 20);
+        expect(typeof response.score).toBe('number')
+      })
 })
+
+
