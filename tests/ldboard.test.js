eae3cc227e0d28c0b1921d9f4171f6cd1019771a
Morenikeji Bello <caeserselassie@gmail.com>
2021-09-16 17:45:00 +0100

    Write test for getScores function in ldboard

--- tests/ldboard.test.js
+++ tests/ldboard.test.js
@@ -1,5 +1,6 @@
 import * as ldAPI from '../src/API/ldboard'
 import 'regenerator-runtime'
+import { it } from '@jest/globals'
 
 jest.mock('../src/API/ldboard')
 
@@ -18,7 +19,50 @@ describe('Store new score', () => {
     it('expects second argument to be a number', async () => {
         const response = await ldAPI.store('A name', 20);
         expect(typeof response.score).toBe('number')
-      })
+    })
+})
+
+describe('Get score from leaderboard', () => {
+    ldAPI.getScores = jest.fn().mockImplementation(() => {
+        
+    const data = {
+        result: [
+            {
+                user: 'Jack Sparrow',
+                score: 20,
+            },
+            {
+                user: 'Peter Parker',
+                score: 35,
+            },
+            {
+                user: 'Goretti',
+                score: 15,
+            },
+        ]
+    }
+
+    return Promise.resolve({
+        json: () => Promise.resolve(data),
+    });
+
+
+    })
+
+    it('returns an array', async () => {        
+
+        const output = await ldAPI.getScores()
+        const response = await output.json();
+        expect(response.result).toBeInstanceOf(Array)
+    })
+
+    it('returns an object as an element in the array', async () => {        
+
+        const output = await ldAPI.getScores()
+        const response = await output.json();
+        expect(response.result[1]).toBeInstanceOf(Object)
+    })
+    
 })
 
 
