450ef1e297bcc23a4c93e3265d6980202b64a62a
Morenikeji Bello <caeserselassie@gmail.com>
2021-09-17 12:41:20 +0100

    Write tests for setConfig function

--- src/API/ldboard.js
+++ src/API/ldboard.js
@@ -1,15 +1,17 @@
 const apiKey = 'KdmaTO7csEf9o0JujaCi';
 const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${apiKey}/scores`;
 
-const setConfig = (user, score) => ({
-  method: 'POST',
-  mode: 'cors',
-  headers: {
-    'Content-Type': 'application/json',
-    Accept: 'application/json',
-  },
-  body: JSON.stringify({ user, score }),
-});
+const setConfig = (user, score) => {
+    return ({
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+        },
+        body: JSON.stringify({ user, score }),
+    });
+}
 
 const store = (player, score) => fetch(url, setConfig(player, score));
 
@@ -18,6 +20,5 @@ const getScores = () => fetch(url);
 export {
   store,
   getScores,
-  apiKey,
-  url,
+  setConfig
 };
--- tests/ldboard.test.js
+++ tests/ldboard.test.js
@@ -1,8 +1,6 @@
 import * as ldAPI from '../src/API/ldboard';
 import 'regenerator-runtime';
 
-jest.mock('../src/API/ldboard');
-
 describe('Store new score in leaderboard API', () => {
   ldAPI.store = jest.fn().mockImplementation((user, score) => ({
     user, score,
@@ -55,3 +53,51 @@ describe('Get scores data from leaderboard', () => {
     expect(response.result[1]).toBeInstanceOf(Object);
   });
 });
+
+describe('setConfig', () => {
+    let config = ldAPI.setConfig('keji', 30);
+
+    it('expects fetch configuration to be an Object', () => {        
+        expect(config).toBeInstanceOf(Object)
+    })
+
+    it('expects method to be post', () => {        
+        expect(config.method).toBe('POST')
+    })
+
+    it('expects mode to be CORS enabled', () => {        
+        expect(config.mode).toBe('cors')
+    })
+
+    it('expects headers to be an object', () => {        
+        expect(config.headers).toBeInstanceOf(Object)
+    })
+
+    it('expects content-type to be json', () => {        
+        expect(config.headers['Content-Type']).toBe('application/json')
+    })
+
+    it('expects accept response type to be json', () => {        
+        expect(config.headers.Accept).toBe('application/json')
+    })
+
+    it('expects fetch body to be an Object', () => {        
+        expect(JSON.parse(config.body)).toBeInstanceOf(Object)
+    })
+
+    it('expects user value in body to be a string', () => {        
+        expect(typeof JSON.parse(config.body).user).toBe('string')
+    })
+
+    it('expects user value in body to be keji', () => {        
+        expect(JSON.parse(config.body).user).toBe('keji')
+    })
+
+    it('expects score value in body to be a number', () => {        
+        expect(typeof JSON.parse(config.body).score).toBe('number')
+    })
+
+    it('expects score value in body to be 20', () => {        
+        expect(JSON.parse(config.body).score).toBe(30)
+    })
+})
